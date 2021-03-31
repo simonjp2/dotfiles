73148b91ccf32b228a1f1b161bbf0ef7ce69719f
Jason Simon <simonjp2@gmail.com>
2021-03-31 09:11:38 -0400

    Add joke shortcut...just for fun

--- zsh/configs/aliases.zsh
+++ zsh/configs/aliases.zsh
@@ -9,3 +9,7 @@ alias cp='cp -iv'
 alias grep='grep --color=auto -E'
 
 alias brewup='brew update && brew bundle'
+
+# Get me a random joke, just for fun
+# https://icanhazdadjoke.com/api#fetch-a-random-dad-joke
+alias joke='http --json --print b https://icanhazdadjoke.com/'
