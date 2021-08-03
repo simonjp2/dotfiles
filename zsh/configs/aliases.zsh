277dba48f7e7a9e017711cc405d3587eb18246c2
Jason Simon <simonjp2@gmail.com>
2021-08-03 08:02:42 -0400

    Feat: Remove custom python function

--- zsh/configs/aliases.zsh
+++ zsh/configs/aliases.zsh
@@ -14,13 +14,3 @@ alias brewup='brew update && brew bundle'
 # https://icanhazdadjoke.com/api#fetch-a-random-dad-joke
 alias joke='http --json --print b https://icanhazdadjoke.com/'
 
-# use custom python
-function python {
-    local install_dir="/usr/local/opt/python@3.9/libexec/bin" 
-    local me="${0}"
-    if [[ ! -d "${install_dir}" ]]; then
-        printf "Unable to find %s. Check python alias: %s" "${install_dir}" "${me}"
-    fi
-    
-    "${install_dir}"/python "${@}"
-}
